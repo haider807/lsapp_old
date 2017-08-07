a3035bfbeb432ef5dd7460f80e031f23d672770a
DESKTOP-VEII1P2\Usman <haider807@gmail.com>
2017-08-06 22:10:43 -0700

    login and register working as well as user id save properly

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -52,6 +52,7 @@ class PostsController extends Controller
         $post = new Post();
         $post->title = $request->input('title');
         $post->body = $request->input('body');
+        $post->user_id = auth()->user()->id;
         $post->save();
         return redirect('/posts')->with('success','Post Created');
     }
@@ -98,6 +99,7 @@ class PostsController extends Controller
         $post = Post::find($id);
         $post->title = $request->input('title');
         $post->body = $request->input('body');
+        //$post->user_id = auth()->user()->id;
         $post->save();
         return redirect('/posts')->with('success','Post Updated');
     }
@@ -112,6 +114,6 @@ class PostsController extends Controller
     {
         $post=Post::find($id);
         $post->delete();
-        return redirect('/posts')->with('success','Post Deleted');
+        return redirect('/posts')->with('success','Post Deleclearted');
     }
 }
